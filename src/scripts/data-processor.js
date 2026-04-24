be35cda0c592c7aa74685eb633c5abbad0fbecac
Brandon Fitzpatrick <113076498+BrandonFitzpatricc@users.noreply.github.com>
2026-04-24 12:01:01 -0400

    Write processUserAddress function in the data processor module

--- src/scripts/data-processor.js
+++ src/scripts/data-processor.js
@@ -2,8 +2,15 @@ const processWeatherInfo = (weatherInfo) => {
   return weatherInfo.days;
 };
 
+const processUserAddress = (userAddress) => {
+  return [
+    userAddress.addresses[0].address.localName,
+    userAddress.addresses[0].address.countrySubdivisionCode,
+  ];
+};
+
 const processUserCoordinates = (userCoordinates) => {
   return [userCoordinates.coords.latitude, userCoordinates.coords.longitude];
 };
 
-export { processWeatherInfo, processUserCoordinates };
+export { processWeatherInfo, processUserAddress, processUserCoordinates };
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,11 +2,11 @@ import "../stylesheets/custom-reset.css";
 import "../stylesheets/style.css";
 
 import { fetchWeatherInfo, fetchUserAddress, getUserCoordinates } from "./data-retriever";
-import { processWeatherInfo, processUserCoordinates } from "./data-processor";
+import { processWeatherInfo, processUserAddress, processUserCoordinates } from "./data-processor";
 
 const userCoordinates = processUserCoordinates(await getUserCoordinates());
 
-const userAddress = await fetchUserAddress(userCoordinates);
+const userAddress = processUserAddress(await fetchUserAddress(userCoordinates));
 console.log(userAddress);
 
 const coordinatesWeatherInfo = processWeatherInfo(
