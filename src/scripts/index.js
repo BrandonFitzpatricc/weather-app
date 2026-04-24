3544a9a97a1e431d862e48ec6fcc0131a073551f
Brandon Fitzpatrick <113076498+BrandonFitzpatricc@users.noreply.github.com>
2026-04-24 14:17:28 -0400

    Rewrite fetch function test in index module

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -13,12 +13,17 @@ import {
   processUserCoordinates,
 } from "./data-processor";
 
-const userCoordinates = processUserCoordinates(await getUserCoordinates());
+getUserCoordinates().then(async (coordinates) => {
+  coordinates = processUserCoordinates(coordinates)
+  const userAddress = processUserAddress(await fetchUserAddress(coordinates));
+  console.log(userAddress);
 
-const userAddress = processUserAddress(await fetchUserAddress(userCoordinates));
-console.log(userAddress);
+  const coordinatesWeatherInfo = processWeatherInfo(
+    await fetchWeatherInfo(userAddress),
+  );
+  console.log(coordinatesWeatherInfo);
+});
 
-const coordinatesWeatherInfo = processWeatherInfo(
-  await fetchWeatherInfo(userCoordinates),
-);
-console.log(coordinatesWeatherInfo);
+fetchWeatherInfo(["Shirley", "New York"]).then(weatherInfo => {
+  console.log(processWeatherInfo(weatherInfo));
+});
