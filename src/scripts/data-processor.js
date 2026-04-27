10f001c81842ff780f4477b85bb13e98cc8ebbd7
Brandon Fitzpatrick <113076498+BrandonFitzpatricc@users.noreply.github.com>
2026-04-27 11:13:25 -0400

    Modify processUserAddress to create and save a location object for the address being processed

--- src/scripts/data-processor.js
+++ src/scripts/data-processor.js
@@ -1,13 +1,15 @@
+import { addLocation } from "./location-manager";
+
 const processWeatherInfo = (weatherInfo) => {
   return weatherInfo.days;
 };
 
 const processUserAddress = (userAddress) => {
-  userAddress = userAddress.addresses[0].address
-  return {
-    city: userAddress.localName,
-    state: userAddress.countrySubdivisionCode,
-  };
+  userAddress = userAddress.addresses[0].address;
+  const city = userAddress.localName;
+  const state = userAddress.countySubdivisionCode;
+  addLocation(city, state, true);
+  return { city, state };
 };
 
 export { processWeatherInfo, processUserAddress };
