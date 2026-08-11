6977bbdd811632008bc04ad0e1be107139bd139a
Brandon Fitzpatrick <113076498+BrandonFitzpatricc@users.noreply.github.com>
2026-08-11 15:30:06 -0400

    Split style.css up into multiple stylesheets

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,9 @@
 import "../stylesheets/custom-reset.css";
-import "../stylesheets/style.css";
+import "../stylesheets/universal.css";
+import "../stylesheets/header.css";
+import "../stylesheets/weather-info.css";
+import "../stylesheets/locations-sidebar.css";
+import "../stylesheets/new-location-prompt.css";
 
 import { startup } from "./application/application-initializer";
 
